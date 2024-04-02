fb256b6bb1ee8c754116cdedce705ca7a5707b3a
CocoYaa <157320361+CocoYaa@users.noreply.github.com>
2024-04-02 17:23:16 -0400

    1

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,16 @@
-console.log("lets see if this remains in your code")
+    
+
+$(document).keyup(function(){
+    
+    var a1=$('#f1').text()
+    var a2=$('#f2').text()
+    var a3=$('#f3').text()
+    var a4=$('#f4').text()
+
+    if(a1=='e' &&a2=='e'&&a3=='a'&&a4=='i'){
+      //alert('Enjoy the class!')
+      //$('#games').hide();
+      //$('#bodys').show();
+      window.location.replace("content.html");
+    }
+  })
